5bd85b63a03f804267a771bf638e24ffb9eafc3a
zhangchengcheng <627395160@qq.com>
2018-04-12 17:59:53 +0800

    'file_upload'

---
+++ src/path.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+
+let objMulter = multer({dest: './www/upload'});
+
+let server = express();
+server.use(objMulter.any());
+server.post('/', function (req, res) {
+    let ext = path.parse(req.files[0].originalname).ext;
+    let newName = req.files[0].path + ext;
+    fs.rename(req.files[0].path, newName, function (err) {
+        if (!err) {
+            res.send('上传成功');
+        } else {
+            res.send('上传失败');
+        }
+        res.end();
+    })
+});
+
+server.listen(8998);
+
--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const fs = require('fs');
+const expressStatic = require('express-static');
+const bodyParser = require('body-parser'); // 不能解析上传的文件
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const jade = require('jade');
+const ejs = require('ejs');
+const multer = require('multer');          // 可以处理上传文件
 
+let objMulter = multer({dest: './www/upload/'});
 let server = express();
 
-server.use('/a.html', function(req, res) {
-    res.send('abc');
-    res.end();
+let keys = [];
+for (let i = 0; i < 1000000; i++) {
+    keys.push('keys' + Math.random());
+}
+server.use(cookieParser('fsafsafjfosajfoiaslf')); // 解析cookie 添加秘钥
+server.use(cookieSession({                        // 使用session
+    name: 'ssid',
+    keys: keys,
+    maxAge: 20*3600*1000
+}));
+server.use(objMulter.any()); // 解析POST数据
+server.use(bodyParser.urlencoded({extended: false})); // 解析POST数据
+
+server.use('/', function(req, res, next) {
+    console.log(req.query, req.body, req.cookies, req.session, req.files);
 });
+
+server.use(expressStatic('./www'));   // 静态数据
 
 server.listen(8999);
